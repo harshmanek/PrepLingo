e2cb6928d944bbe150ce1eb50c5a542867ae4dfb
Harsh Manek <harshmanek786@gmail.com>
2025-05-06 10:54:49 +0530

    First Commit.

---
+++ src/main/java/com/harsh/preplingo/PrepLingoApplication.java
@@ -0,0 +1,13 @@
+package com.harsh.preplingo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PrepLingoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PrepLingoApplication.class, args);
+    }
+
+}
